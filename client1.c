993bc5756670954a36a3ae9baf3ec5ce4cee4362
Determinant179 <travkinpa@mail.ru>
2021-12-15 15:46:11 +0300

    Add structure

--- client1.c
+++ client1.c
@@ -8,43 +8,58 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define PORT	 8080
-#define MAXLINE 1024
-
 // Driver code
-int main() {
-	int sockfd;
-	char buffer[MAXLINE];
-	char *hello = "Hello from client";
-	struct sockaddr_in	 servaddr;
+int main()
+{
 
-	// Creating socket file descriptor
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
+    const char *file_names[] = {"file1.txt", "file2.txt", "file3.txt"};
+    char output[2048] = "";
 
-	memset(&servaddr, 0, sizeof(servaddr));
-	
-	// Filling server information
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(PORT);
-	servaddr.sin_addr.s_addr = INADDR_ANY;
-	
-	int n, len;
-	
-	sendto(sockfd, (const char *)hello, strlen(hello),
-		0, (const struct sockaddr *) &servaddr,
-			sizeof(servaddr));
-	printf("Hello message sent.\n");
-		
-	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-				MSG_WAITALL, (struct sockaddr *) &servaddr,
-				&len);
-	buffer[n] = '\0';
-	printf("Server : %s\n", buffer);
+    for (size_t i = 0; i < sizeof(file_names) / sizeof(file_names[0]); i++)
+    {
+        char cmd[2048] = "wc -l ";
+        char output_line[2048] = "";
 
-	close(sockfd);
-	return 0;
+        strcat(cmd, file_names[i]);
+
+        FILE *fp;
+        if ((fp = popen(cmd, "r")) == NULL)
+        {
+            fprintf(stderr, "\n<CLIENT 1>\nError while popen\n\n");
+            return 3;
+        }
+
+        fgets(output_line, 2048, fp);
+
+        strcat(output, output_line);
+    }
+
+    int sockfd;
+    char buffer[2048];
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        printf("<CLIENT 1>\nSocket creation failed\n\n");
+    }
+
+        printf("<CLIENT 1>\nSocket was created\n\n");
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(8080);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    int n, len;
+
+    sendto(sockfd, output, 2048, 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    n = recvfrom(sockfd, (char *)buffer, 2048, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
+    buffer[n] = '\0';
+    printf("<CLIENT 1>\nMessage from server:\n%s\n\n", buffer);
+
+    close(sockfd);
+    return 0;
 }
-
--- server.c
+++ server.c
@@ -8,52 +8,63 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define PORT	 8080
-#define MAXLINE 1024
-
 // Driver code
-int main() {
-	int sockfd;
-	char buffer[MAXLINE];
-	char *hello = "Hello from server";
-	struct sockaddr_in servaddr, cliaddr;
-	
-	// Creating socket file descriptor
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	memset(&servaddr, 0, sizeof(servaddr));
-	memset(&cliaddr, 0, sizeof(cliaddr));
-	
-	// Filling server information
-	servaddr.sin_family = AF_INET; // IPv4
-	servaddr.sin_addr.s_addr = INADDR_ANY;
-	servaddr.sin_port = htons(PORT);
-	
-	// Bind the socket with the server address
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
-			sizeof(servaddr)) < 0 )
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	int len, n;
+int main()
+{
 
-	len = sizeof(cliaddr); //len is value/resuslt
+    FILE *fp;
+    if ((fp = popen("ls -p | grep -v /", "r")) == NULL)
+    {
+        fprintf(stderr, "\n<SERVER>\nError while popen\n\n");
+        return 3;
+    }
 
-	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-				MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-				&len);
-	buffer[n] = '\0';
-	printf("Client : %s\n", buffer);
-	sendto(sockfd, (const char *)hello, strlen(hello),
-		0, (const struct sockaddr *) &cliaddr,
-			len);
-	printf("Hello message sent.\n");
-	
-	return 0;
+    char output[2048] = "";
+    char line[2048];
+
+    int i = 0;
+    while (1)
+    {
+        if (fgets(line, 2048, fp) == NULL)
+            break;
+        i++;
+        strcat(output, line);
+    }
+
+    int sockfd;
+    char buffer[2048];
+    struct sockaddr_in servaddr, cliaddr;
+
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        printf("<SERVER>\nSocket creation failed\n\n");
+    }
+
+    printf("<SERVER>\nSocket was created\n\n");
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(8080);
+
+    // Bind the socket with the server address
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("<SERVER>\nBind failed\n\n");
+    }
+
+    int len, n;
+
+    len = sizeof(cliaddr); // len is value/resuslt
+
+    n = recvfrom(sockfd, (char *)buffer, 2048, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+    buffer[n] = '\0';
+    printf("<SERVER>\nMessage from client1:\n%s\n\n", buffer);
+    sendto(sockfd, output, 2048, 0, (const struct sockaddr *)&cliaddr, len);
+
+    return 0;
 }
-
